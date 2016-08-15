f0e93380647d67a9b217ec0ae403fb33557b1ce7
capitan <capitan@capitans-Mac.local>
2016-08-15 14:06:32 +0300

    Solid

--- solid/single_responsibility.rb
+++ solid/single_responsibility.rb
@@ -1,68 +1,74 @@
-# Read first https://en.wikipedia.org/wiki/SOLID_(object-oriented_design)
-# Parts of code were taken from https://robots.thoughtbot.com/back-to-basics-solid
+# The Single Responsibility Principle is the most abstract of the bunch. It helps keep classes and methods small and maintainable. In addition to keeping classes small and focused it also makes them easier to understand.
 
-# Single Responsibility Principle
-# Hint: The Single Responsibility Principle is the most abstract of the bunch. It helps keep classes and methods small and maintainable. In addition to keeping classes small and focused it also makes them easier to understand.
+# While we all agree that focusing on a single responsibility is important, it’s difficult to determine what a class’s responsibility is. Generally, it is said that anything that gives a class a reason to change can be viewed as a responsibility. By change I am talking about structural changes to the class itself (as in modifying the code in the class’s file, not the object’s in-memory state).
+# In the below class we have a single command interface that processes commission payments for deals. At first glance the class seems simple enough, but let’s look at reasons we might want to change this class. Any change in how we calculate commissions would require a change to this class. We could introduce new commission rules or strategies that would cause our calculate_commission method to change. For instance, we might want to vary the percentage based on deal amount. Any change in the steps required to mark a deal as processed in the mark_deal_processed method would result in a change in the file as well. An example of this might be adding support for sending an email summary of a specific person’s commissions after marking a deal processed. The fact that we can identify multiple reasons to change signals a violation of the Single Responsibility Principle.
 
 class DealProcessor
-  # Create instance with collection deals should be processed
+  attr_reader :deals
+
   def initialize(deals)
     @deals = deals
   end
 
-  # Process each deal
-  # Pass calculated commision and deal and create comiision instance
   def process
-    @deals.each do |deal|
-      Commission.create(deal: deal, amount: calculate_commission)
+    deals.each do |deal|
+      # Here we calculate commission and create instance of Commission
+      Commission.create(deal: deal, amount: calculate_commission(deal))
       mark_deal_processed
     end
   end
 
   private
+
   def mark_deal_processed
     # Implementation
   end
 
-  def calculate_commission
-    @deal.dollar_amount * 0.05
+  def calculate_commission(deal)
+    deal.amount * 0.05
   end
 end
 
-# DealProcessor means Hey I process deal and I don't wan't to do any think.
-# Calculate commission method breaks this idea. Just take out it.
+class Commission
+  # Implementation
+end
+
+# Solution
 
 class DealProcessor
-  # Create instance with collection deals should be processed
+  attr_reader :deals
+
   def initialize(deals)
     @deals = deals
   end
 
-  # Process each deal
-  # Now we call Class that calculate commision and create commision instance
-  # If commision created and marked processed return it
   def process
-    @deals.each do |deal|
-      commision = CommisionCalcilator.crate_comission(deal)
-      commision if mark_deal_processed
+    deals.each do |deal|
+      # Now we call calculator in one operation, all logic now in it
+      CommissionCalculator.create_commission(deal) if mark_deal_processed
     end
   end
 
   private
+
   def mark_deal_processed
     # Implementation
   end
 end
 
-class CommisionCalculator
-  # Now we get deal and calculate amount of commision and return object with it
-  def self.create_commision(deal)
-    Commision.new(deal: deal, amount: calculate(deal))
+class CommissionCalculator
+  def self.create_commission(deal)
+    Commission.new(deal: deal, amount: calculate(deal))
   end
 
   private
+
   def self.calculate(deal)
-    deal.dollar_amount * 0.05
+    deal.amount * 0.05
   end
+end
+
+class Commission
+  # Implementation
 end
 
