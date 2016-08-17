e08a0ed5ca4b3ae40df06bb4c56a0ffa34848b4b
Marat Khusnetdinov <marat@khusnetdinov.ru>
2016-08-17 15:53:00 +0300

    Factory pattern

--- README.md
+++ README.md
@@ -135,7 +135,9 @@ The builder pattern is an object creation software design pattern. Unlike the ab
 
 #### Factory pattern
 
-[See example] ()
+In class-based programming, the factory method pattern is a creational pattern that uses factory methods to deal with the problem of creating objects without having to specify the exact class of the object that will be created. This is done by creating objects by calling a factory method—either specified in an interface and implemented by child classes, or implemented in a base class and optionally overridden by derived classes—rather than by calling a constructor.
+
+[See example] (https://github.com/evncom/ruby.fundamental/blob/master/patterns/creational/factory.rb)
 
 #### Prototype pattern
 
---
+++ patterns/creational/factory.rb
@@ -0,0 +1,39 @@
+# Factory pattern is one of most used design pattern in Object oriented design.
+# This type of design pattern comes under creational pattern as this pattern
+# provides one of the best ways to create an object. In Factory pattern, we
+# create object without exposing the creation logic to the client and refer to
+# newly created object using a common interface.
+
+# Classes witch are used in factory
+class Rectangle
+  # Implementation
+end
+
+class Square
+  # Implementation
+end
+
+class Circle
+  # Implementation
+end
+
+# Factory
+class ShapeFactory
+  def get_shape(type)
+    case type
+    when :rectangle then Rectangle.new
+    when :square then Square.new
+    when :circle then Circle.new
+    end
+  end
+end
+
+# Usage
+
+shape_factory = ShapeFactory.new
+square = shape_factory.get_shape :square
+
+
+
+
+
